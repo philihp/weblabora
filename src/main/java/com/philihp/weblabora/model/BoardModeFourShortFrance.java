34e4d3a48dd66cf700669e8d85e8a3e820576fa9
Philihp Busby <philihp@gmail.com>
2012-12-06 05:05:19 -0500

    stone/grape now come out and are displayed at proper rounds for short 4/3 game

--- src/main/java/com/philihp/weblabora/model/BoardModeFourShortFrance.java
+++ src/main/java/com/philihp/weblabora/model/BoardModeFourShortFrance.java
@@ -1,7 +1,9 @@
 package com.philihp.weblabora.model;
 
 import static com.philihp.weblabora.model.TerrainTypeEnum.PLAINS;
+import static com.philihp.weblabora.model.Wheel.Position.D;
 import static com.philihp.weblabora.model.Wheel.Position.E;
+import static com.philihp.weblabora.model.Wheel.Position.F;
 import static com.philihp.weblabora.model.Wheel.Position.H;
 import static com.philihp.weblabora.model.Wheel.Position.K;
 
@@ -140,17 +142,18 @@ public class BoardModeFourShortFrance extends BoardMode {
 
 	@Override
 	public int grapeActiveOnRound() {
-		return 8;
+		return 4;
 	}
 
 	@Override
 	public int stoneActiveOnRound() {
-		return 13;
+		return 6;
 	}
 
 	@Override
 	public void setWheelTokens(Wheel wheel) {
-		wheel.grape.setPosition(H);
+		wheel.grape.setPosition(D);
+		wheel.stone.setPosition(F);
 	}
 
 	@Override
